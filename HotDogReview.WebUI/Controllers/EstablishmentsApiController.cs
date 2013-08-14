22ca830de6da92d1e4a11c9b4050a0128beea62d
Phillippe Santana <phillippelevidad@gmail.com>
2013-08-14 14:53:28 -0300

    Criando o API Controlle de establishments

---
+++ HotDogReview.WebUI/Controllers/EstablishmentsApiController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using HotDogReview.WebUI.Models;
+
+namespace HotDogReview.WebUI.Controllers
+{
+    public class EstablishmentsApiController : ApiController
+    {
+        // Fields.
+        private HotDogReviewEntities db = new HotDogReviewEntities();
+
+        // IDisposable methods.
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
